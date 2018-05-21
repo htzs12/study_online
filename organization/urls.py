632917ac7e573abf01324c4fb5af119767986f72
htzs12 <www.htzs@qq.com>
2018-05-22 06:48:37 +0800

    user_fav

--- organization/urls.py
+++ organization/urls.py
@@ -3,7 +3,7 @@ __date__ = '18-5-20 上午8:03'
 
 from django.urls import path,re_path
 
-from .views import OrgView,AddUserAskView,OrgHomeView,OrgCourseView
+from .views import OrgView,AddUserAskView,OrgHomeView,OrgCourseView,OrgDescView,OrgTeacherView,AddFavView
 
 
 app_name = 'organization'
@@ -11,7 +11,9 @@ app_name = 'organization'
 urlpatterns = [
     path('list/',OrgView.as_view(),name='org_list'),
     path('add_ask/',AddUserAskView.as_view(),name='add_ask'),
-        # home页面,取纯数字
     re_path('home/(?P<org_id>\d+)/', OrgHomeView.as_view(), name="org_home"),
     re_path('course/(?P<org_id>\d+)/', OrgCourseView.as_view(), name="org_course"),
+    re_path('desc/(?P<org_id>\d+)/', OrgDescView.as_view(), name="org_desc"),
+    re_path('teacher/(?P<org_id>\d+)/', OrgTeacherView.as_view(), name="org_teacher"),
+    path('add_fav/', AddFavView.as_view(), name="add_fav"),
 ]
